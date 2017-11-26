331ac11f339a184325ac303193ad1ac10bf8a1f1
Camjones114 <32233004+Camjones114@users.noreply.github.com>
2017-11-26 00:05:14 -0800

    Add files via upload

---
+++ milestone2/Assets/DefKit/Scripts/Triangle.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System;
+/*
+ * Name: Cameron Jones
+ * Title: Triangle.cs
+ */
+namespace DefKit
+{
+    // Stores three points indices consisting ona triangle and it's id
+    [Serializable]
+    public struct Triangle
+    {
+
+        public int id;
+        public int pointAid;
+        public int pointBid;
+        public int pointCid;
+
+        public Triangle(int id, int idA, int idB, int idC)
+        {
+            this.id = id;
+            this.pointAid = idA;
+            this.pointBid = idB;
+            this.pointCid = idC;
+        }
+
+        public int GetById(int id)
+        {
+            if (id == 0)
+                return pointAid;
+            else if (id == 1)
+                return pointBid;
+            else if (id == 2)
+                return pointCid;
+
+            return -1;
+        }
+    }
+}
